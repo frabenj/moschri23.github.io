b1620bf203a5abe666e833a44186a512199122a3
moschri23 <31492767+moschri23@users.noreply.github.com>
2017-09-07 09:33:01 +1000

    Update pong.js

--- pong.js
+++ pong.js
@@ -51,3 +51,17 @@ Player.prototype.render = function() {
 Computer.prototype.render = function() {
   this.paddle.render();
 };
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.x_speed = 0;
+  this.y_speed = 3;
+  this.radius = 5;
+}
+
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "#000000";
+  context.fill();
+};
